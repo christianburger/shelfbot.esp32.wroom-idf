9eef115a362a903d563bea053274a878e115911d
Christian Burger <christian_burger@yahoo.com>
2026-05-17 14:02:19 -0300

    fixing includes and environment.

--- components/shelfbot/shelfbot.hpp
+++ components/shelfbot/shelfbot.hpp
@@ -1,40 +1,6 @@
 // [file name]: shelfbot.hpp
 #pragma once
-
-#include <cstring>
-#include <cinttypes>
-#include <memory>
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
-#include "esp_system.h"
-#include "nvs_flash.h"
-#include "esp_event.h"
-#include "esp_netif.h"
-#include "mdns.h"
-#include "esp_sntp.h"
-
-#include "wifi_station.hpp"
-#include "motor_control.hpp"
-#include "../http_server/include/http_server.hpp"
-#include "led_control.hpp"
-#include "sensor_control.hpp"
-#include "sensor_common.hpp"
-#include "firmware_version.hpp"
-
-#include <rcl/rcl.h>
-#include <rclc/rclc.h>
-#include <rclc/executor.h>
-#include <rmw_microros/rmw_microros.h>
-#include <rmw_microros/ping.h>
-
-#include <std_msgs/msg/int32.h>
-#include <std_msgs/msg/bool.h>
-#include <std_msgs/msg/float32.h>
-#include <std_msgs/msg/float32_multi_array.h>
-
-#include <ctime>
+#include <idf_c_includes.hpp>
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){ESP_LOGE(TAG, "RCL error in %s: %d", #fn, (int)temp_rc);}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){ESP_LOGW(TAG, "RCL soft error in %s: %d", #fn, (int)temp_rc);}}
